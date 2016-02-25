93eb0b1450bec6cda398e558022663c4bc95a506
Bob Main <bobmain49@gmail.com>
2016-02-24 18:05:09 -0800

    e

--- dynamicGraph.js
+++ dynamicGraph.js
@@ -1,10 +1,15 @@
 var svgElement = "http://www.w3.org/2000/svg";
-var xlines = 25;
-var ylines = 25;
 
-var unitBreak = 5;//bold lines every...
-var thin = 1;
-var thick = 2;
+// let's keep it square //remove...
+var xlines = 40;
+var ylines = 40;
+
+var boldBreak1 = 5;//bold lines every...
+var boldBreak2 = 10;//extra bold lines every...
+var thin = .25;
+var bold1 = .75;
+var bold2 = 1.5;
+
 
 var margin = 5;//in percent
 var oL = 1; //percent of lines that overlap to create crossing at edges
@@ -12,13 +17,15 @@ var xgap = (100 - (margin*2)) / xlines;
 var ygap = (100 - (margin*2)) / ylines;
 
 for (var i = 0; i <= ylines; i++){
-  if((i)%unitBreak===0){ var lineW = thick;} else { var lineW = thin;}
+  if((i)%boldBreak1===0){ var lineW = bold1;} else { var lineW = thin;}
+  if((i)%boldBreak2===0){ var lineW = bold2;} //else { var lineW = thin;}
   createEl("line",[["x1",margin-oL+"%"],["y1",i*ygap+margin+"%"]
     ,["x2",100-margin+oL+"%"],["y2",i*ygap+margin+"%"],["stroke-width",lineW]])
 }
 
 for (var i = 0; i <= xlines; i++){
-  if((i)%unitBreak===0){ var lineW = thick;} else { var lineW = thin;}
+  if((i)%boldBreak1===0){ var lineW = bold1;} else { var lineW = thin;}
+  if((i)%boldBreak2===0){ var lineW = bold2;} //25else { var lineW = thin;}
   createEl("line",[["x1",i*xgap+margin+"%"],["y1",margin-oL+"%"]
     ,["x2",i*xgap+margin+"%"],["y2",100-margin+oL+"%"],["stroke-width",lineW]])
 }
@@ -30,3 +37,11 @@ function createEl(type,att){
     }
     document.getElementById("mainSVG").appendChild(newObj);
   }
+
+/*
+----------Workflow...-----------------------------------------------
+
+
+----------Not important ideas...------------------------------------
+-- gradient margins so they fad off the page....
+*/
