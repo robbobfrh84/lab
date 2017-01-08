95567d49480130864950438d593f781534b80db9
Bob Main <bobmain49@gmail.com>
2017-01-08 09:28:35 -0800

    jul

--- TI-83-RPG/semantic_main.js
+++ TI-83-RPG/semantic_main.js
@@ -1,19 +1,26 @@
-$(function() {
-    $('.ui.dropdown').dropdown();
-});
+var currentPage = document.getElementById('landingPageText');
 
 $(document).ready(function() {
     $('.ui.menu .ui.dropdown').dropdown({
       on: 'hover'
     });
-    $('.ui.menu a.item')
-      .on('click', function() {
+    $('.ui.menu a.item').on('click', function() {
         $(this)
           .addClass('active')
           .siblings()
-          .removeClass('active')
-        ;
-      })
-    ;
+          .removeClass('active');
+    });
+    $('#backStoryIcon').on('click', function() {
+        $('#backStoryText')
+          .removeClass('hidden');
+        $('#landingPageText')
+          .addClass('hidden');
+    })
+    $('#TI-83-RPG').on('click', function() {
+        $('#backStoryText')
+          .addClass('hidden');
+        $('#landingPageText')
+          .removeClass('hidden');
+    })
   })
 ;
