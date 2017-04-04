03a436665b88604a74d9c1668dcaa630f3bdce03
Bob Main <bobmain49@gmail.com>
2017-04-04 10:06:41 -0700

    Adding nested dynamic html arrays

--- Independancy/-Lab/zDynamic_HTML_Lab/for.js
+++ Independancy/-Lab/zDynamic_HTML_Lab/for.js
@@ -2,53 +2,40 @@ class htmlFor {
 
   main (Obj, startTime = window.performance.now()) {
     var elements = document.querySelectorAll('[for]') // Grabs All tags with 'for' element
-    for (const elm of elements) { // Loop through all tags with 'for' element
-      const [ node, condition, parent ] = elm.getAttribute('for').split(' ') // Grab 'for' elm string, split and declare node(ind/arr or key/obj), parent is html var
-      this.checkTags(Obj, elm, node, condition, parent, false)
+    for (let i = elements.length-1; i >= 0; i--) { // Loop through all tags with 'for' element. Needs to be in reverse cuz nested loops need to run first.
+      const [ node, condition, parent ] = elements[i].getAttribute('for').split(' ') // Grab 'for' elm string, split and declare node(ind/arr or key/obj), parent is html var
+      this.checkTags(Obj, elements[i], node, condition, parent)
     }
     console.log((window.performance.now() - startTime) + ' milliseconds')
   }
 
-  checkTags (Obj, elm, node, condition, parent, child) {
+  checkTags (Obj, elm, node, condition, parent) {
     let tags = elm.childNodes.length // Snatch this value as a seperate because length of childnodes will change dynamically, creating INFINATE LOOPS OF PERIL!
-    // console.log('head:', elm)
     for (const i in Obj[parent]) { // Loop through all indices/keys within the Object
+      //---------
+      console.log(elm, parent, i, Obj[parent][i])
+      //----------
       for (let j = 0; j < tags; j++) { // loop through all tags within element.
         let tag = elm.childNodes[j]
-        // if (tag.childElementCount > 0) {
-        //   console.log('tag.localName: ', tag.localName)
-        //   this.checkTags(Obj, tag, node, condition, parent, true)
-        // }
-        // if (tag.outerHTML) {
-        //   console.log(tag.outerHTML, tag.innerHTML.split(' ')[0] !== '\n')
-        // }
-        if (tag.outerHTML && tag.innerHTML.split(' ')[0] !== '\n') { // there's extra DOM stuff we dont' need, This will only duplicate tags we created.
-          // console.log('in', tag.outerHTML)
-          let tArr = this.spacing(tag.innerHTML.split(' '), node, Obj[parent][i])
-          if (this.show(tag, i, Obj[parent])) {
-            this.newTag(elm, tag.localName, tArr.join(' '), child)
+        if (tag.contentEditable) { // there's extra DOM stuff we dont' need, This will only duplicate tags we created.
+          let textArr = this.spacing(tag.innerHTML.split(' '), node, Obj[parent][i])
+          if (this.show(tag, i, Obj[parent])) { // returns bool, if we should show this tag, this.show also does the parsing for dynamic vars.
+            this.newTag(elm, tag, textArr.join(' '))
           }
         }
       }
     }
     for (let i = 0; i < tags; i++) elm.removeChild(elm.childNodes[0])
   }
-// ok so here's where i lef it
-// I think i understand why we're getting the texthtml in the browser... we append it to the parent as TEXT, NOT an actual element....
-//
-  newTag (parent, tag, innerHTML, isChild) {
-    // console.log('c1: ', parent, tag, innerHTML, isChild)
-    let child = document.createElement(tag)
-    // if (isChild) {
-    //   child.appendChild(document.createTextNode(innerHTML))
-    // } else {
-      child.appendChild(document.createTextNode(innerHTML))
-    // }
+
+  newTag (parent, tag, innerHTML) {
+    let child = document.createElement(tag.localName)
+    child.innerHTML = innerHTML
     parent.appendChild(child)
   }
 
   show (tag, i, l, indices) {
-    if (tag.getAttribute('indices')) indices = tag.getAttribute('indices').split(' ') // ^ If the tag has the custom indices attribute, catch it as array here.
+    if (tag.getAttribute('indices')) indices = tag.getAttribute('indices').split(' ') // If the tag has the custom indices attribute, catch it as array here.
     if (!indices) return true // if the tag doesn't have that attr, return 1 and make it.
     for (const j of indices) {
       if (j[0] === 's') if (!(i >= parseInt(j.substr(1)))) return false // start/don't show tags until
@@ -75,18 +62,15 @@ class htmlFor {
     }
     return arr
   }
-
 }
 
 (function (){
   var Obj = {
-    arr: ['First', 'Second', 'Third'],
+    arr: ['1st', '2nd', '3rd'],
     arr2: ['purple', 'green', 'blue', 'orange', 'black', 'blue', 'pink', 'white'],
-    arr3: ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight',
-      'nine', 'ten', 'eleven', 'twelve', 'thirdteen', 'fourteen', 'fifteen', 'sixteen',
-      'seventeen', 'eighteen', 'nineteen', 'twenty'],
+    arr3: ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirdteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen', 'twenty'],
     arr4: ['xXx', 'yYy'],
-    nestArr: ['nest1', 'nest2','nest3'],
+    arr5: ['nest1', 'nest2'],
     obj: {
       Country: 'Canada',
       Color: 'red',
@@ -98,12 +82,10 @@ class htmlFor {
 })()
 
 /********** ToDo **********
-- looks like nested TAG, not for=s don't even work!
 - Objects
-- convet to class
+- double check is 'of' the correct condition, also you don't have 'in'(or whichever the other is. )
 - remove global vars ... ? but why, i should understand why (closures)
 - checkout the spaces function again see if it can be a switch.
-- nested arrs...
 - I think it's important to control the html vars in the .html page. So add that functionality.
 - Clean, Note, convert more and simpler examples.
 // ADD: arr.substr(index) It'll chop off first index,, not zeroth.
