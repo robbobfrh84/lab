dddea6bdbd8bdaefd86706769abcbe97afa549d0
Bob Main <bobmain49@gmail.com>
2017-05-02 14:17:39 -0700

    updating mongoDB notes

--- men-crud-basics/server.js
+++ men-crud-basics/server.js
@@ -1,36 +1,47 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-// const MongoClient = require('mongodb').MongoClient
+const MongoClient = require('mongodb').MongoClient
+// const assert = require('assert');
+const url = 'mongodb://localhost:27017/men-crud-basics'
+var db
 
 app.use(bodyParser.urlencoded({ extended: true }))
-app.listen(3000, () => { console.log('listening on 3000') })
+// app.listen(3000, () => { console.log('listening on 3000') })
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html')
-})
+app.get('/', (req, res) => { res.sendFile(__dirname + '/public/index.html') })
+
+MongoClient.connect(url, (err, database) => {
+  if (err) return console.log(err)
+  // assert.equal(null, err)
+  app.listen(3000, () => {
+    console.log('Connected to Server and listening on 3000')
+  })
+  db = database
+  // database.close();
+  db.collection('quotes').find().toArray((err,results) => {
+    console.log('-------!!!!!!! >>>> results: ', results)
+  })
+});
+
+// app.get('/', (req, res) => {
+//   //var ??? not const or let???
+//   var cursor = db.collectioin('quote').find()
+//   console.log('cursor: ', cursor)
+// })
 
 app.post('/quotes', (req, res) => {
   console.log(req.body)
+  db.collection('quotes').save(req.body, (err, results) => {
+    if (err) console.log(err)
+    console.log('saved to database')
+    res.redirect('/')
+  })
 })
 
-// MongoClient.connect('link-to-mongodb', (err,database) => {
-//
-// })
 
-var MongoClient = require('mongodb').MongoClient
-  , assert = require('assert');
 
-// Connection URL
-console.log(__dirname)
-var url = 'mongodb://localhost:27017/men-crud-basics'
-// Use connect method to connect to the Server
-MongoClient.connect(url, function(err, db) {
-  assert.equal(null, err);
-  console.log("Connected correctly to server");
 
-  db.close();
-});
 
 
 
