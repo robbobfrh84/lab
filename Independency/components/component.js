28ff211e2bacccc66a469c20bbe0a25ad5ac076f
Bob Main <bobmain49@gmail.com>
2017-05-24 22:06:07 -0700

    checking out other methods of importing .html files

--- Independency/components/component.js
+++ Independency/components/component.js
@@ -1,22 +1,9 @@
-// var pages = []
-//
-// var links = document.getElementsByTagName('link')
-// // console.log('links: ', links)
-var localID = '?'
-
 class componentJS {
 
-  // constructor () {
-  //   this.localID = []
-  // }
-
   newElm (component, id) {
-    this.id = id
-    //this.localID = [1]
     const importDoc = document.currentScript.ownerDocument
     let proto = Object.create(HTMLElement.prototype)
     const events = this.events
-    // let componentDoc = this.componentDoc
     proto.createdCallback = function() {
       const template = importDoc.querySelector(id)
       const clone = document.importNode(template.content, true)
@@ -24,23 +11,12 @@ class componentJS {
       root.appendChild(clone)
       for (const event of events) {
         let newEvent = template.content.getElementById(event.id)
-        // newEvent.id = id + newEvent.id
         newEvent.addEventListener(event.type,
           event.method.bind(this, this), false)
       }
       this.appendChild(template.content)
-      // componentDoc(this)
     }
     document.registerElement(component, {prototype: proto})
-  }
-
-  componentDoc (content) {
-    // console.log('componentDoc', content.getElementsByTagName('*'))
-    let x = content.getElementsByTagName('*')
-    console.log('----------', x)
-    //if (!this.localID) this.localID = [1,2,3]
-
-    console.log(localID)
   }
 
   addEvent (type, id, method) {
