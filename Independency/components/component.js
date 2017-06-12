1e179a0a5ed992198beeb06ba37b157bdf9fbe3f
Bob Main <bobmain49@gmail.com>
2017-06-12 11:07:01 -0700

    tinkering with storing root values

--- Independency/components/component.js
+++ Independency/components/component.js
@@ -4,27 +4,20 @@ class Component {
     this.events = []
     this.component = component
     this.id = id
-    this.content = {}
   }
 
   newElm (events = this.events, id = this.id) {
     let proto = Object.create(HTMLElement.prototype)
     const importDoc = document.currentScript.ownerDocument
     const template = importDoc.querySelector(id)
-    // this.content = template.content
     proto.attributeChangedCallback = function() {
       const root = this.createShadowRoot()
       let clone = document.importNode(template.content, true)
-
-      this.content = clone
-      console.log('-', clone.getElementById('title'))
-
       for (const e of events) {
         let newEvent = clone.getElementById(e.id)
-        newEvent.addEventListener(e.type,
-          e.method.bind(this, root), false)
-          // e.method.bind(this), false)
-          // e.method.bind(this, this), false)
+        newEvent.addEventListener(e.type, ()=>{
+          e.method(root)
+        })
       }
       root.appendChild(clone)
       // this.appendChild(clone)
@@ -38,19 +31,23 @@ class Component {
       //     //e.method.bind(this), this), false)
       // }
       // this.appendChild(template.content)
-
     }
     document.registerElement(this.component, {prototype: proto})
   }
 
   addEvent (type, id, method) {
-    // if (!this.events) this.events = [] // in constr
     this.events.push( {'type': type, 'method': method, 'id': id} )
   }
 
-  getData (tag) {
+  // setRoot (root) {
+  //   console.log(root)
+  //   this.root = root
+  // }
+
+  data (root) {
     // this will be problematic when being used for multiple elements...
-    return JSON.parse(document.getElementsByTagName(tag)[0].getAttribute('serve'))
+    return JSON.parse(root.host.attributes.serve.nodeValue)
+    // return JSON.parse(document.getElementsByTagName(this.component)[0].getAttribute('serve'))
   }
 
 }
