35781b9961e3849e6115e314679e9221ac06ce2f
Bob Main <bobmain49@gmail.com>
2017-06-13 20:25:39 -0700

    - removed notes

--- Independency/components/component.js
+++ Independency/components/component.js
@@ -4,7 +4,6 @@ class Component {
     this.events = []
     this.component = component
     this.id = id
-    // this.test = ((component)=>{ this.newElm(component) })()
   }
 
   newElm (events = this.events, id = this.id, onLoaded = this.onLoaded) {
@@ -12,20 +11,7 @@ class Component {
     const importDoc = document.currentScript.ownerDocument
     const template = importDoc.querySelector(id)
     proto.attributeChangedCallback = function () {
-      // let root;
-      // let newRoot = true
-      // if (!this.shadowRoot) {
-      //   root = this.createShadowRoot()
-      // } else {
-      //   root = this.shadowRoot
-      //   newRoot = false
-      // }
-      // if (this.shadowRoot) {
-      //   this.shadowRoot = null
-      // }
       const root = this.createShadowRoot()
-      // console.dir(root)
-
       let clone = document.importNode(template.content, true)
       const data = JSON.parse(this.getAttribute('serve'))
       for (const e of events) {
@@ -37,25 +23,14 @@ class Component {
       if (onLoaded) {
         document.addEventListener("DOMContentLoaded", ()=>{ onLoaded(root, data) })
       }
-      // this.appendChild(template.content)
       root.appendChild(clone)
-      // // vvv !!!
-      // if(!document.root) document.root = []
-      // document.root.push(root)
-      // // ^^^ !!!
-
     }
     document.registerElement(this.component, {prototype: proto})
-
   }
 
   addEvent (type, id, method) {
     this.events.push( {'type': type, 'method': method, 'id': id} )
   }
-
-  // onload () {
-  //
-  // }
 
   getData (root) {
     return JSON.parse(root.host.attributes.serve.nodeValue)
