1b2f760b74b471e0bb39c71d8ec433c0fd4c650e
Bob Main <bobmain49@gmail.com>
2017-08-04 15:32:50 -0700

    file move

---
+++ bracket-generator/static-brackets.js
@@ -0,0 +1,7 @@
+var arnold = [
+  { seed: 1, name: 'Terminator II: Demolition Day Starring more and more'},
+  { seed: 2, name: 'Total Recall'},
+  { seed: 3, name: 'Terminator'},
+  { seed: 4, name: 'Kindergarden Cop asdf asdf asdf asdf asdf'},
+  { seed: 5, name: 'Predator'}
+]
---
+++ builtInJS.js
@@ -0,0 +1,64 @@
+/******** String/Arr Parsing *******/
+arr.includes(value) // >Booloan. If the value is represented in the array.
+str.substr(index+) // >string. custs string at the index. returns 2nd half, str dosn't change., oddly, it's not a 0th index, but first.
+arr.splice(i, 1) // removes index at i. the second param tells how many additional beyond.
+arr.indexOf(value) // >Integer. can even be entire object in array!
+arr.join(' ') // >String. joins array with spaces between indices.
+str.split('-') // >Arr. Splits str at -
+str.split(/[.\[\]]/) // >Arr removes . [ ]
+"Obj.str2[1].more".split(/[.\[\]]/).filter(Boolean) // >Arr removes . [ ] But! with this string it'll leave a dead "", the filter removes that.
+JSON.parse(data) // > Object from string of data.
+
+/******** DataTypes (changing / descovering)*******/
+parseInt(stringLiteralAsNumber) // >Intiger. converts string num to intiger.
+int.toString() // >String. converts int to String
+Array.isArray(obj) // >Bool. wheather or not it's an array or Object. WHY NOT? typeOf, because Arr & Obj both return as Obj's
+
+/******** Objects *******/
+Object.keys(obj) // >array of keys
+Object.keys(obj).indexOf('key') // >Intiger. find the index of a key within an object.
+
+/******** DOM interactions *******/
+document.querySelectorAll('[for]') // >Array of DOM objects with for attribute
+tag.hasAttribute('attribute') // >Bool if it has the attr
+tag.getAttribute('attribute') // >String Gets html's tag element DOM attributes.
+tag.Some_Attribue = 'value' // many html attributes can be added dot.notation.style
+let att = document.createAttribute("class"); // create custom Attribute
+  att.value = "democlass";
+  tag.setAttributeNode(att);
+tag.removeChild(tag.childNodes[0]) // removes first child within html's tag.
+while (tag.hasChildNodes()) { tag.removeChild(tag.lastChild) } // remove ALL children
+var newTag = document.createElement('html') // creats new tag
+var textElm = document.createTextNode('Some text') // creates text elm
+newTag.appendChild(textElm) // puts text elm inside new tag
+parent.appendChild(newTag) // puts tag into DOM
+let clone = tag.cloneNode(true) //cloneNode
+document.onreadystatechange = function(){ if(document.readyState === 'complete'){ ...}} //function firest after document has been loaded... one of many ways to do this ... btw...
+
+/******** RegExp *******/
+new RegExp(aBunchOfcharsArr.join('|')).test(toSearchThroughArr) // >Bool if ALL conditions are met. test() is NOT poop in this.
+
+/******** quick functions *******/
+(function (){ console.log('will auto fire') })() // self-invoking function
+ES6 (()=>{ console.log('will auto fire') })() // self-invoking function
+
+/******** events *******/
+Event Listener Types = [ click, keyup, keydown, keypress ]
+inputId.addEventListener('keypress', function (e) { if (e.keyCode == 13) { ?? }}) // ??=add stuff to do when "enter" is pressed while filling out input tag.
+
+/******** loops *******/
+array.forEach(function(value, i) { console.log(value, i) }) // value is object/array/var, whatever of ARRAY, i is index
+
+/******** time *******/
+const startTime = window.performance.now()
+console.log((window.performance.now() - startTime) + ' milliseconds')
+OR window.date.now()
+
+/*********************/
+/****     ES6+    ****/
+/*********************/
+
+/******** Arrow Funcs *******/
+( p1, p2 )=>{ expression }
+p1 => { expresison }
+()=> expression // auto return expression ! only: if not parameter
