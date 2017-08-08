14125b088eec0ea5e55b5b57d103f393e2c620c5
Bob Main <bobmain49@gmail.com>
2017-08-07 18:29:12 -0700

    added loneJS-site project to repo

---
+++ loneJS-site/data.js
@@ -0,0 +1,10 @@
+var _DATA = {
+  home: {
+    test: 'Yo planet!',
+    array: [ 'How', 'are', 'you?' ],
+  },
+  page2: {
+    test: "'Sup Earthlings???",
+    array: [ 'Wanna', 'hug?' ],
+  }
+}
---
+++ loneJS-site/loneJS-lib/bower_components/shadydom/externs/shadydom.js
@@ -0,0 +1,14 @@
+/**
+ * @fileoverview Externs to upstream to closure compiler
+ * @externs
+ */
+
+/**
+ * @type {string}
+ */
+Element.prototype.slot;
+
+/**
+ * @type {Object}
+ */
+Node.prototype.__shady;
