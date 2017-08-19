bcd2aad62632af1ab65f5d4fd330d56fe8adc09c
Bob Main <bobmain49@gmail.com>
2017-08-18 19:31:04 -0700

    adding page holders

--- loneJS-site/data.js
+++ loneJS-site/data.js
@@ -2,7 +2,9 @@ var _DATA = {
   home: {
     title: "Currently Under Developement",
     bar: "What is LoneJS?",
-    text1: "LoneJS is a front-end plugin that utilizes the built in power of HTML5, CSS3, and JavaScript to bring a fully functioning Single Page Appliation fully loaded on the front-end without any dependencies for most browers."
+    text1: "LoneJS is a front-end plugin that utilizes the built in power of HTML5, CSS3, and JavaScript to bring a fully functioning Single Page Appliation fully loaded on the front-end without any dependencies for most browers.",
+    bar2: "Who is it for?",
+    text2: "Well..."
   },
   component: {
     title: "Component Collection"
