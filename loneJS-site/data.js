33e21f60d421a235fca9f2e816071fba827216bd
Bob Main <bobmain49@gmail.com>
2017-08-18 19:35:38 -0700

    prepping for life push

--- loneJS-site/data.js
+++ loneJS-site/data.js
@@ -1,6 +1,6 @@
 var _DATA = {
   home: {
-    title: "Currently Under Developement",
+    title: "Welcome to LoneJS",
     bar: "What is LoneJS?",
     text1: "LoneJS is a front-end plugin that utilizes the built in power of HTML5, CSS3, and JavaScript to bring a fully functioning Single Page Appliation fully loaded on the front-end without any dependencies for most browers.",
     bar2: "Who is it for?",
