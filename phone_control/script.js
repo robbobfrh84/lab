9136ab145b4ec01695ea56f8415feca85edf38aa
Bob Main <bobmain49@gmail.com>
2018-11-20 16:31:22 -0600

    adding phone_controller

---
+++ phone_control/script.js
@@ -0,0 +1,15 @@
+window.onload = function(){
+
+  for (const elm of document.querySelectorAll('.btn')) {
+    elm.onclick = function(){
+      const bg = elm.style.backgroundColor || 'rgba(0,0,0,0.1)'
+      elm.style.backgroundColor = bg === 'green' ? 'rgba(0,0,0,0.1)'  : 'green'
+      data.textContent = elm.id
+    }
+  }
+
+  all.ontouchmove = function(){
+    console.log(this)
+    data.textContent = 'MOVED'
+  }
+}
