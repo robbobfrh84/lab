19adf6f8bb87f6227b4042fe064246cb3c1912e4
Bob Main <bobmain49@gmail.com>
2018-11-30 16:30:38 -0600

    finished raw multi-signed... waiting for game now

---
+++ firebase/Multi_signin_challenge/config.js
@@ -0,0 +1,8 @@
+var config = {
+  apiKey: "AIzaSyAkKPA4oLAxjp41NPctKSwXBx6M5EjsPeU",
+  authDomain: "fir-multi-signin.firebaseapp.com",
+  databaseURL: "https://fir-multi-signin.firebaseio.com",
+  projectId: "fir-multi-signin",
+  storageBucket: "",
+  messagingSenderId: "1077809638225"
+};
