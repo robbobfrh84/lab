fc211deddf1868c043b7ce06c427d9116dca2363
robbobfrh84 <bobmain49@gmail.com>
2023-01-02 17:37:35 -0800

    lasypush

--- hanna/README.md
+++ hanna/README.md
@@ -1,5 +1,8 @@
 # To Do ✅ 🟡
-- fix placement to be ratio.
+- ✅fix placement to be ratio.
+- 🟡add Y to touch
+- handle XY max min leaving touch element 'tracker'
+  - Looks after  testing that there isn't an 'touchoff' event. so just use imgX/imgY greater than. 
 - Test touch match 
 - Create hover and touch effect
 ~~~
--- hanna/app.js
+++ hanna/app.js
@@ -17,34 +17,8 @@ function setVars() {
   console.log('imgX,imgY:',imgX,imgY);
 }
 
-
-/* * * * *    🖥️ 🐭 USER EVENTS 🐭 🖥️      * * * * */
-
-tracker.onmousemove = (e)=>{
-
-  const oX = e.offsetX
-  // const rX = hanna.leftEye.rangeX 
-  // const rangeTotX = Math.abs(rX.min) + Math.abs(rX.max)
-  const rXmin = (hanna.leftEye.rangeX.min * imgX)
-  const rXmax = (hanna.leftEye.rangeX.max * imgX)
-  console.log('rXmin, rXmax:',rXmin, rXmax);
-  const rangeTotX = Math.abs(rXmin) + Math.abs(rXmax)
-  let x = ((rangeTotX*oX) / imgX) + rXmin
-  leftEye.style.left = x+"px"
-
-  const oY = e.offsetY
-  // const rY = hanna.leftEye.rangeY
-  const rYmin = (hanna.leftEye.rangeY.min * imgY)
-  const rYmax = (hanna.leftEye.rangeY.max * imgY)
-  // const rangeTotY = Math.abs(rY.min) + Math.abs(rY.max)
-    const rangeTotY = Math.abs(rYmin) + Math.abs(rYmax)
-
-  let y = ((rangeTotY*oY) / imgY) + rYmin
-  leftEye.style.top = y+"px"
-}
-
-tracker.onmouseout = ()=>{
-  console.log(' - OUT');
+function resetFace() {
+  console.log(' - resetFace()');
   leftEye.style.left = "0px"
   leftEye.style.top = "0px"
 }
@@ -53,22 +27,46 @@ tracker.onmouseout = ()=>{
 // },500) // clearInterval(recenter)
 
 
+/* * * * *    🖥️ 🐭 USER EVENTS 🐭 🖥️      * * * * */
+
+tracker.onmouseout = resetFace
+tracker.onmousemove = (e)=>{
+  const oX = e.offsetX
+  const rXmin = (hanna.leftEye.rangeX.min * imgX)
+  const rXmax = (hanna.leftEye.rangeX.max * imgX)
+  console.log('mouse e.offsetX:',e.offsetX);
+
+  const rangeTotX = Math.abs(rXmin) + Math.abs(rXmax)
+  let x = ((rangeTotX*oX) / imgX) + rXmin
+  leftEye.style.left = x+"px"
+
+  const oY = e.offsetY
+  const rYmin = (hanna.leftEye.rangeY.min * imgY)
+  const rYmax = (hanna.leftEye.rangeY.max * imgY)
+  const rangeTotY = Math.abs(rYmin) + Math.abs(rYmax)
+  let y = ((rangeTotY*oY) / imgY) + rYmin
+  leftEye.style.top = y+"px"
+}
+
+
+
 
 /* * * * *    📱👇 TOUCH USER EVENTS 👇📱     * * * * */
 
-tracker.touchmove = (e)=>{
-  // From: https://stackoverflow.com/questions/33548926/how-to-detect-touchmove-length-offsets
-  // - See LAST non-JQ answer. 
+tracker.ontouchend = resetFace
+tracker.ontouchmove = (e)=>{ 
+  // From: https://stackoverflow.com/questions/33548926/how-to-detect-touchmove-length-offsets - See LAST non-JQ answer. 
   var touch = e.touches[0] || e.changedTouches[0];
   var realTarget = document.elementFromPoint(touch.clientX, touch.clientY);
-  e.offsetX = touch.clientX-realTarget.getBoundingClientRect().x;
+  e.offsetX = touch.clientX-realTarget.getBoundingClientRect().x
   e.offsetY = touch.clientY-realTarget.getBoundingClientRect().y
+  console.log('e.offsetX:',e.offsetX);
 
-  console.log('touchmove', e  )
   const oX = e.touches[0].clientX
-  const rX = hanna.leftEye.rangeX
-  const rangeTotX = Math.abs(rX.min) + Math.abs(rX.max)
-  let x = ((rangeTotX*oX) / imgX) + rX.min
+  const rXmin = (hanna.leftEye.rangeX.min * imgX)
+  const rXmax = (hanna.leftEye.rangeX.max * imgX)
+  const rangeTotX = Math.abs(rXmin) + Math.abs(rXmax)
+  let x = ((rangeTotX*oX) / imgX) + rXmin
   leftEye.style.left = x+"px"
 }
 document.addEventListener('touchmove', (e)=>{e.preventDefault()}, { passive: false })
