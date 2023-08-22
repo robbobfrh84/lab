9e4189269961e1c1c14ece6bf8f710ada95a1d3c
robbobfrh84 <bobmain49@gmail.com>
2023-08-22 11:27:18 -0700

    lasypush

---
+++ website/links.js
@@ -0,0 +1,35 @@
+const pages = [
+  { folder: 'tour_de_france', route: 'website/pages/', mobile: true,
+    tweet: 'https://twitter.com/BobMain49/status/1428839915484221442',
+    medium: 'https://medium.com/@bobmain49'
+  },
+  { folder: 'mouse_line_warp', route: 'website/pages/', mobile: true, },
+  { folder: 'darwins_divs', route: 'website/pages/', mobile: true, },
+  { folder: 'erosion', route: 'website/pages/', mobile: true, },
+
+  { folder: 'greg', route: 'website/pages/', mobile: true, },
+  { folder: 'erosion', route: 'website/pages/', mobile: true, },
+
+  { folder: 'rand', route: 'website/pages/', mobile: true, },
+  { folder: 'black_dot', route: 'website/pages/', mobile: true, },
+  { folder: 'emojie_earth_sun_moon', route: 'website/pages/', mobile: true, },
+  { folder: 'bit_by_bit', route: 'website/pages/', mobile: true, },
+  { folder: 'hanna', route: 'website/pages/', mobile: true, },
+
+  // { folder: 'test_for_a_longer_link', route: 'website/pages/', mobile: true,
+  //   tweet: 'https://twitter.com/BobMain49/status/1428839915484221442',
+  //   medium: 'https://medium.com/@bobmain49',
+  //   youtube: 'https://youtube.com',
+  //   threads: 'https://www.threads.net/',
+  // },
+  
+]
+
+const other = [
+  
+]
+
+const local = [
+
+]
+
