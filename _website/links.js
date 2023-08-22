acfc3943c2cec6d64b4222d22cf5702b17ea1981
robbobfrh84 <bobmain49@gmail.com>
2023-08-22 10:54:22 -0700

    lasypush

--- _website/links.js
+++ _website/links.js
@@ -5,6 +5,12 @@ const pages = [
     medium: 'https://medium.com/@bobmain49'
   },
   { folder: 'darwins_divs', route: '_website/pages/', mobile: true, },
+  { folder: 'test_for_a_longer_link', route: '_website/pages/', mobile: true,
+    tweet: 'https://twitter.com/BobMain49/status/1428839915484221442',
+    medium: 'https://medium.com/@bobmain49',
+    youtube: 'https://youtube.com',
+    threads: 'https://www.threads.net/',
+  },
 ]
 
 const other = [
