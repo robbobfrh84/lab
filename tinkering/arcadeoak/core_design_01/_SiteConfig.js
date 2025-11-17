76fb6275a3e4e03bd29f76afc1cdfef856c5d70c
robbobfrh84 <bobmain49@gmail.com>
2025-11-17 07:42:27 -0800

    lasypush

--- tinkering/arcadeoak/core_design_01/_SiteConfig.js
+++ tinkering/arcadeoak/core_design_01/_SiteConfig.js
@@ -4,13 +4,7 @@ const _SiteConfig = {
   site: "development",
   basePaths: ['development', 'arcademain_dev', 'production'],
   firebase: {
-    apiKey: "AIzaSyAi7RJN5i3bEHQ5tbIG24LB1MRgV3DNieM",
-    authDomain: "dev-arcade-main.firebaseapp.com",
-    projectId: "dev-arcade-main",
-    storageBucket: "dev-arcade-main.firebasestorage.app",
-    messagingSenderId: "607521377270",
-    appId: "1:607521377270:web:8470c591cb0ca5f4ef95d7",
-    measurementId: "G-LY0JVFG5FT"
+// Removed for google security
   }
 }
 _SiteConfig.tests = Tests
