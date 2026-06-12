3a5b8e6011aaee3b43d05113b1f0159ac88dbce6
robbobfrh84 <bobmain49@gmail.com>
2026-06-11 18:16:25 -0700

    lasypush

--- tinkering/toc/TOC_2026__Hidden_Values/config.js
+++ tinkering/toc/TOC_2026__Hidden_Values/config.js
@@ -12,10 +12,12 @@ const regionsObj = [
   // TOC - Hidden Values 2026
   { name: 'Highland', color: '#de891aff', font: 'white'},
   { name: 'Woodland', color: '#6B8E4E', font: 'white'},
+  { name: 'Forest', color: '#557938', font: 'white'},
   { name: 'Valley', color: '#a74391ff', font: 'white'},
   { name: 'Coastal', color: '#e6d972ff', font: 'black'},
   { name: 'Islands', color: '#2EB8D4', font: 'black'},
   { name: 'Prairie', color: '#D4B857', font: 'black'},
+  { name: 'Stepp', color: '#dead1a', font: 'white'},
   { name: 'Forest', color: '#2D5A2E', font: 'white'},
   { name: 'Mountain', color: '#9099A2', font: 'black'},
   { name: 'Desert', color: '#E6C77E', font: 'black'},
